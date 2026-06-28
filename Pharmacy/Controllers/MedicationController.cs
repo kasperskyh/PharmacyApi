6279d0f654cb9b8d73f9d40f64311298919044c7
Kacper Hunia <116276383+kasperskyh@users.noreply.github.com>
2026-06-28 17:52:09 +0200

    Podłączono bazę danych SQLite

--- Pharmacy/Controllers/MedicationController.cs
+++ Pharmacy/Controllers/MedicationController.cs
@@ -1,11 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
-using Pharmacy.Database;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Pharmacy.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class MedicationController : ControllerBase
+namespace PharmacyApi.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
+    public class MedicationController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Medication>> Get()
+        {
+            return Ok(MedicationData.GetMedications());
+        }
 
+        [HttpGet("{id}")]
+        public ActionResult<Medication> GetById(int id)
+        {
+            var medication = MedicationData.GetMedications().FirstOrDefault(m => m.Id == id);
+            if (medication == null)
+            {
+                return NotFound();
+            }
+            return Ok(medication);
+        }
+    }
 }
